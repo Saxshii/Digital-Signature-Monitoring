77d4965643d16348c6392b066e854df9b85ec992
Sakshi <sakshiii.t17@gmail.com>
2026-06-24 11:30:39 +0530

    day 1

---
+++ data.js
@@ -0,0 +1,19 @@
+const employees = [
+  {
+    name: "Sakshi",
+    email: "sakshixhere@gmail.com",
+    expiryDate: "2026-08-10"
+  },
+  {
+    name: "Shruti Singh",
+    email: "Shrutixhere@gmail.com",
+    expiryDate: "2026-07-05"
+  },
+  {
+    name: "Amit Kumar",
+    email: "amit@gmail.com",
+    expiryDate: "2026-12-20"
+  }
+];
+
+module.exports = employees;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+const employees = require("./data");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  const employeeData = employees.map((emp) => {
+    const expiry = new Date(emp.expiryDate);
+
+    const daysLeft = Math.ceil((expiry - today) / (1000 * 60 * 60 * 24));
+
+    let status = "Active";
+
+    if (daysLeft <= 60) {
+      status = "Expiring Soon";
+    }
+
+    return { ...emp, daysLeft, status };
+  });
+
+  const total = employeeData.length;
+  const expiring = employeeData.filter(
+    (emp) => emp.status === "Expiring Soon",
+  ).length;
+
+  res.render("dashboard", {
+    employees: employeeData,
+    total,
+    expiring,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server Running");
+});
